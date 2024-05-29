eadc810812e9ca6453f45f4ba5a1c4891e1bddfb
Mariyiya <151542716+Mariyiya@users.noreply.github.com>
2024-05-29 23:07:04 +0700

    add2

--- pindie-backend/middlewares/cors.js
+++ pindie-backend/middlewares/cors.js
@@ -3,7 +3,7 @@ const allowedCors = [
     'https://students-projects.ru',
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://pindie-back-end.nomorepartiesco.ru/'
+    'https://pindie-back-end.nomorepartiesco.ru'
   ];
 
   function cors(req, res, next) {
